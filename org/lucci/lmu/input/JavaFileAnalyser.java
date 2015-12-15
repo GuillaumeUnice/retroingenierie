a3d3bfdefd2f239b19eca965e2d5e50981f14d58
Guillaume BORG <guillaume.unice@gmail.com>
2015-12-15 12:54:59 +0100

    myfirst commit

---
+++ org/lucci/lmu/input/JavaFileAnalyser.java
@@ -0,0 +1,13 @@
+package org.lucci.lmu.input;
+
+import org.lucci.lmu.Model;
+
+public class JavaFileAnalyser extends ModelFactory {
+
+	@Override
+	public Model createModel(byte[] data) throws ParseError, ModelException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
